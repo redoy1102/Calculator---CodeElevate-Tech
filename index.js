6a7c72742a2c1f07a5f500834d46b94952b0c767
redoy1102 <hridoyvlog9@gmail.com>
2024-05-09 17:43:43 +0700

    After getting a result whenever I am inserting another value it just …

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// let added = false;
+let equalEventCounter = 0;
+
 function setOperator(operator)
 {
     let string = document.querySelector('input[name="display"]').value;
@@ -6,5 +7,26 @@ function setOperator(operator)
     
     if (lastCharacter != operator) {
         document.querySelector('input[name="display"]').value += operator;
+    }
+}
+function handleEqual(expression)
+{
+    document.querySelector('input[name="display"]').value = eval(expression);
+    equalEventCounter++;
+}
+function AC()
+{
+    document.querySelector('input[name="display"]').value = '';
+}
+function numberPad(value)
+{
+    if (equalEventCounter != 0)
+    {
+        document.querySelector('input[name="display"]').value = ''; 
+        document.querySelector('input[name="display"]').value += value;
+        equalEventCounter = 0;
+    }
+    else {
+        document.querySelector('input[name="display"]').value += value;
     }
 }
